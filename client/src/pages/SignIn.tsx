9c83550128d0fcf04cb78f9fc598666760130d1b
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-03-11 11:02:21 +0300

    feat:signin page added with redirect to Home Page

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,6 +1,39 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const SignIn = () => {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = values;
+
+  const handleChange = (username) => (event) => {
+    setValues({ ...values, [username]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const signUser = await axios.post("/api/login", {
+        username,
+        password,
+      });
+      
+      if (signUser) {
+        setValues({ ...values, username: "", password: "" });
+        toast.success("Sign in successfully, redirecting to home page...");
+        window.location.href = "/";
+      }
+    } catch (error) {
+      
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
     <div>
       <div className="bg-gray-100 flex items-center justify-center h-screen">
@@ -12,9 +45,11 @@ const SignIn = () => {
                 Username
               </label>
               <input
-                type="username"
+                type="text"
                 id="username"
                 name="username"
+                value={username}
+                onChange={handleChange("username")}
                 className="mt-1 block w-full rounded-md border border-black shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
               />
             </div>
@@ -26,12 +61,15 @@ const SignIn = () => {
                 type="password"
                 id="password"
                 name="password"
+                value={password}
+                onChange={handleChange("password")}
                 className="mt-1 block w-full rounded-md border border-black shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
               />
             </div>
             <div className="mb-4">
               <button
                 type="submit"
+                onClick={handleSubmit}
                 className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
               >
                 Sign In
@@ -39,7 +77,7 @@ const SignIn = () => {
             </div>
           </form>
           <div className="text-sm text-center text-gray-600">
-            Don't have an account?{" "}
+            Already have an account ?{" "}
             <a href="#" className="text-indigo-500 hover:text-indigo-700">
               Sign Up
             </a>
