dce626b01c7f60a11738e43162f12ffd9de6daa9
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-03-11 16:22:32 +0300

    feat: routing mechanism for app is implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,60 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home.tsx";
 import SignUp from "./pages/SignUp.tsx";
 import SignIn from "./pages/SignIn.tsx";
+import AdminLogin from "./pages/admin/AdminLogin.tsx";
+import AdminDashboard from "./pages/admin/AdminDashboard.tsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track if user is logged in
+  const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false); // State to track if admin is logged in
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const handleAdminLogin = () => {
+    setIsAdminLoggedIn(true);
+  };
+
+  const handleAdminLogout = () => {
+    setIsAdminLoggedIn(false);
+  };
   return (
     <>
       <ToastContainer />
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={isLoggedIn ? <Home /> : <Navigate to="/signin" />}
+          />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn onLogin={handleLogin} />} />
+          <Route
+            path="/admin"
+            element={<AdminLogin onLogin={handleAdminLogin} />}
+          />
+          <Route
+            path="/adminPage"
+            element={
+              isAdminLoggedIn ? <AdminDashboard /> : <Navigate to="/admin" />
+            }
+          />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
