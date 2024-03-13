38f174d4db99c27f792430f97d0f441ddfb4b26f
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-03-13 15:10:50 +0300

    feat: integrate the Canvas component to Home Page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Canvas from "./Canvas.tsx";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex items-center justify-center mt-10">
+      <Canvas width={600} height={600} />
     </div>
   );
 };
