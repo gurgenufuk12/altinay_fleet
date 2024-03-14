d5b9815e35351f1b0729b959917efc3123ce7f89
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-03-14 17:16:50 +0300

    feat: add route and function for adding new target to the robot

--- server/controllers/robots.js
+++ server/controllers/robots.js
@@ -15,3 +15,53 @@ exports.getRobots = async (req, res, next) => {
     });
   }
 };
+exports.addTarget = async (req, res, next) => {
+  try {
+    const {
+      taskName,
+      taskCode,
+      taskPriority,
+      taskPercentage,
+      robotName,
+      lineerVelocity,
+      angularVelocity,
+      targetPosition,
+      targetOrientation,
+      targetExecuted,
+    } = req.body;
+    const robot = await Robot.findOne({ robotName: robotName });
+    if (!robot) {
+      return res.status(400).json({
+        success: false,
+        message: "Robot not found",
+      });
+    }
+    robot.Target = {
+      Position: targetPosition,
+      Orientation: targetOrientation,
+      targetExecuted: targetExecuted,
+    };
+    robot.Task = {
+      taskName: taskName,
+      taskCode: taskCode,
+      taskPriority: taskPriority,
+      taskPercentage: taskPercentage,
+    };
+    robot.robotVelocity = {
+      lineerVelocity: lineerVelocity,
+      angularVelocity: angularVelocity,
+    };
+
+    await robot.save();
+    res.status(200).json({
+      success: true,
+      data: robot,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- server/routes/robots.js
+++ server/routes/robots.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { getRobots } = require("../controllers/robots");
+const { addTarget } = require("../controllers/robots");
+
+
 
 router.get("/getRobots", getRobots);
+router.post("/addTarget", addTarget);
+
 module.exports = router;
