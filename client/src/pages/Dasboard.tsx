0a51de247816bcdf462320139bd1f5a52ff5628f
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-03-15 17:07:44 +0300

    feat: Map Component related path changed

--- client/src/pages/Dasboard.tsx
+++ client/src/pages/Dasboard.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import Map from "./Map.tsx";
+import Map from "../components/Map.tsx";
 import TaskTable from "./tasks/taskTable.tsx";
 import Sidebar from "../components/SideBar.tsx";
 
 const Dasboard = () => {
   return (
     <div className="flex items-center justify-center mt-10">
-      <Sidebar  />
-      <Map width={600} height={600}  />
-      <TaskTable />
+      <Sidebar />
+      <Map width={600} height={600} />
     </div>
   );
 };
