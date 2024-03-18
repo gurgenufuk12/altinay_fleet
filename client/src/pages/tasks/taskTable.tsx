81f9f288d31949b27427999d513a46a29e8e1e75
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-03-18 17:02:12 +0300

    feat:taskTable update with Task Details

--- client/src/pages/tasks/taskTable.tsx
+++ client/src/pages/tasks/taskTable.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import Sidebar from "../../components/SideBar";
+
 interface Task {
   robotName: string;
-  Target: {
+  userName: string;
+  Targets: {
     Position: {
       x: string;
       y: string;
@@ -16,8 +18,8 @@ interface Task {
       z: string;
       w: string;
     };
-    targetExecuted: boolean;
-  };
+    targetExecuted: string;
+  }[];
   Task: {
     taskCode: string;
     taskName: string;
@@ -25,9 +27,8 @@ interface Task {
     taskPriority: string;
   };
 }
-const TaskTable = () => {
+const TaskTable: React.FC = () => {
   const [tasks, setTasks] = React.useState<Task[]>([]);
-
   React.useEffect(() => {
     const intervalId = setInterval(() => {
       retrieveTasks();
@@ -45,31 +46,45 @@ const TaskTable = () => {
   };
 
   return (
-    // show side bar and table of tasks
     <div className="flex items-center justify-center mt-10 ">
       <Sidebar />
       <table className="table-auto">
         <thead>
           <tr>
             <th className="px-4 py-2">Robot Name</th>
+            <th className="px-4 py-2">Given By</th>
             <th className="px-4 py-2">Task Code</th>
             <th className="px-4 py-2">Task Name</th>
             <th className="px-4 py-2">Task Percentage</th>
             <th className="px-4 py-2">Task Priority</th>
-            <th className="px-4 py-2">Target Position</th>
+            <th className="px-4 py-2">Task Details</th>
           </tr>
         </thead>
         <tbody>
           {tasks.map((task, index) => (
             <tr key={index}>
               <td className="border px-4 py-2">{task.robotName}</td>
+              <td className="border px-4 py-2">{task.userName}</td>
               <td className="border px-4 py-2">{task.Task.taskCode}</td>
               <td className="border px-4 py-2">{task.Task.taskName}</td>
               <td className="border px-4 py-2">{task.Task.taskPercentage}</td>
               <td className="border px-4 py-2">{task.Task.taskPriority}</td>
               <td className="border px-4 py-2">
-                {task.Target.Position.x}, {task.Target.Position.y},{" "}
-                {task.Target.Position.z}
+                {task.Targets &&
+                  task.Targets.map((target, idx) => (
+                    <div key={idx}>
+                      <p>
+                        Position: {target.Position.x}, {target.Position.y},{" "}
+                        {target.Position.z}
+                      </p>
+                      <p>
+                        Orientation: {target.Orientation.x},{" "}
+                        {target.Orientation.y}, {target.Orientation.z},{" "}
+                        {target.Orientation.w}
+                      </p>
+                      <p>Target Executed: {target.targetExecuted}</p>
+                    </div>
+                  ))}
               </td>
             </tr>
           ))}
