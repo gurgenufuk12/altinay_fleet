c4387f79ce590c79262dc5b70f4177014433758c
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-03-18 13:47:21 +0300

    feat: add user to the top of sideBar

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,9 +1,40 @@
 import React from "react";
-
+import axios from "axios";
+interface User {
+  id: string;
+  username: string;
+}
 const Sidebar = () => {
+  const [token, setToken] = React.useState<string | null>("");
+  const [user, setUser] = React.useState<User | null>(null);
+  React.useEffect(() => {
+    const token = localStorage.getItem("userData");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("/api/getUserByToken", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUser(res.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, [token]);
   return (
-    <div className="fixed top-0 left-0 h-full w-40 bg-gray-800 text-white">
+    <div className="fixed top-0 left-0 h-full w-60 bg-gray-800 text-white">
       <div className="p-4">
+        <h1 className="text-2xl font-bold">
+          {user ? `Welcome, ${user.username}` : "Welcome"}
+        </h1>
         <ul>
           <li
             className="py-2 hover:bg-gray-700"
