1158e788c3537268d46475489f45feaf811d0ca4
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-03-20 15:21:27 +0300

    feat: add filter to the Task Table depending on the Task Name

--- client/src/pages/tasks/taskTable.tsx
+++ client/src/pages/tasks/taskTable.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import Sidebar from "../../components/SideBar";
+import Filter from "../../assets/filter.png";
 
 interface Task {
   robotName: string;
@@ -28,9 +29,13 @@ interface Task {
     taskPriority: string;
   };
 }
+
 const TaskTable: React.FC = () => {
-  const [tasks, setTasks] = React.useState<Task[]>([]);
-  React.useEffect(() => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [filter, setFilter] = useState<string>("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+  useEffect(() => {
     const intervalId = setInterval(() => {
       retrieveTasks();
     }, 500);
@@ -46,8 +51,26 @@ const TaskTable: React.FC = () => {
     }
   };
 
+  const applyFilter = (taskName: string) => {
+    if (taskName === "All") {
+      setFilter("");
+    } else {
+      setFilter(taskName);
+    }
+    setIsDropdownOpen(false);
+  };
+
+  const clearFilter = () => {
+    setFilter("");
+  };
+
+  const filteredTasks = tasks.filter((task) => {
+    if (!filter) return true;
+    return task.Task.taskName === filter;
+  });
+
   return (
-    <div className="flex items-center justify-center pl-10  bg-orange-100">
+    <div className="flex items-center justify-center pl-10 bg-orange-100">
       <Sidebar />
       <table className="table-auto">
         <thead>
@@ -55,7 +78,55 @@ const TaskTable: React.FC = () => {
             <th className="px-4 py-2">Robot Name</th>
             <th className="px-4 py-2">Given By</th>
             <th className="px-4 py-2">Task Code</th>
-            <th className="px-4 py-2">Task Name</th>
+            <th className="px-4 py-2 relative flex items-center">
+              Task Name
+              <div className="relative inline-block text-left ml-2">
+                <button
+                  type="button"
+                  id="options-menu"
+                  aria-expanded="true"
+                  aria-haspopup="true"
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                >
+                  <img src={Filter} alt="filter" width={20} height={20} />
+                </button>
+                {isDropdownOpen && (
+                  <div
+                    className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="options-menu"
+                  >
+                    <div className="py-1" role="none">
+                      <button
+                        className="text-gray-700 block w-full text-left px-4 py-2 text-sm"
+                        onClick={() => applyFilter("All")}
+                      >
+                        All
+                      </button>
+                      <button
+                        className="text-gray-700 block w-full text-left px-4 py-2 text-sm"
+                        onClick={() => applyFilter("Patrol")}
+                      >
+                        Patrol
+                      </button>
+                      <button
+                        className="text-gray-700 block w-full text-left px-4 py-2 text-sm"
+                        onClick={() => applyFilter("Docking")}
+                      >
+                        Docking
+                      </button>
+                      <button
+                        className="text-gray-700 block w-full text-left px-4 py-2 text-sm"
+                        onClick={() => applyFilter("Docking")}
+                      >
+                        Lift
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </th>
             <th className="px-4 py-2">Task Percentage</th>
             <th className="px-4 py-2">Task Priority</th>
             <th className="px-4 py-2">Task Start Time</th>
@@ -63,7 +134,7 @@ const TaskTable: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          {tasks.map((task, index) => (
+          {filteredTasks.map((task, index) => (
             <tr key={index}>
               <td className="border px-4 py-2">{task.robotName}</td>
               <td className="border px-4 py-2">{task.userName}</td>
