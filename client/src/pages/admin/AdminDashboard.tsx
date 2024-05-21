77b0fcf1748478cfd5dc7fc9c85bc07045ec6d97
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-05-21 18:48:09 +0300

    feat: UI fix

--- client/src/pages/admin/AdminDashboard.tsx
+++ client/src/pages/admin/AdminDashboard.tsx
@@ -169,56 +169,66 @@ const AdminDashboard = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <SideBar />
-      <h1 className="text-3xl font-bold mb-4 ml-10">Admin Dashboard</h1>
-      <div className="flex ml-10">
-        <div className="w-1/2 pr-4">
-          <h2 className="text-xl font-bold mb-2">Admin Users</h2>
-          <div className="overflow-x-auto">
-            <table className="table-auto w-full border-collapse border border-gray-400">
-              <thead>
+      <h1 className="text-3xl font-bold mb-4 ml-14 text-gray-800">
+        Admin Dashboard
+      </h1>
+      <div className="flex flex-wrap justify-around ml-14">
+        <div className="w-full lg:w-1/2 pr-4 mb-8">
+          <h2 className="text-xl font-bold mb-2 text-gray-700">Admin Users</h2>
+          <div className="overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
                 <tr>
-                  <th className="px-4 py-2">Username</th>
-                  <th className="px-4 py-2">Role</th>
-                  <th className="px-4 py-2">Change Role</th>
-                  <th className="px-4 py-2">Actions</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Username
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Role
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Change Role
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Actions
+                  </th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="bg-white divide-y divide-gray-200">
                 {adminUsers.map((user, index) => (
                   <tr
                     key={index}
-                    className={index % 2 === 0 ? "bg-white" : "bg-gray-100"}
+                    className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
                   >
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
                       {user.username}
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
                       {user.user_Role}
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap">
                       <select
                         id={`role_${user.username}`}
                         value={selectedRoles[user.username]}
                         onChange={(event) =>
                           handleRoleChange(event, user.username)
                         }
-                        className="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                        className="block w-full bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 rounded shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                       >
                         <option value="admin">Admin</option>
                         <option value="user">User</option>
                       </select>
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap">
                       <Button
                         onClick={() => saveUserRoleChanges(user.username)}
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
                         title="Save"
-                      ></Button>
+                      />
                       <Button
                         onClick={() => deleteUser(user.username)}
                         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
                         title="Delete"
-                      ></Button>
+                      />
                     </td>
                   </tr>
                 ))}
@@ -226,75 +236,92 @@ const AdminDashboard = () => {
             </table>
           </div>
         </div>
-        <div className="w-1/2 pl-4">
-          <h2 className="text-xl font-bold mb-2">Locations</h2>
-          <div className="overflow-x-auto">
-            <table className="table-auto w-full border-collapse border border-gray-400">
-              <thead>
+        <div className="w-full lg:w-1/2 pl-4 mb-8">
+          <h2 className="text-xl font-bold mb-2 text-gray-700">Locations</h2>
+          <div className="overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
                 <tr>
-                  <th className="px-4 py-2">Location Name</th>
-                  <th className="px-4 py-2">Location Description</th>
-                  <th className="px-4 py-2">Target Position</th>
-                  <th className="px-4 py-2">Actions</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Location Name
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Location Description
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Target Position
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Actions
+                  </th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="bg-white divide-y divide-gray-200">
                 {locations.map((location, index) => (
                   <tr
                     key={index}
-                    className={index % 2 === 0 ? "bg-white" : "bg-gray-100"}
+                    className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
                   >
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
                       {location.locationName}
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
                       {location.locationDescription}
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {location.Target.Position.x}, {location.Target.Position.y}
-                      , {location.Target.Position.z}
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
+                      x : {parseFloat(location.Target.Position.x).toFixed(2)}, y
+                      : {parseFloat(location.Target.Position.y).toFixed(2)}
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap">
                       <Button
                         onClick={() => deleteLocation(location.locationName)}
                         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                      >
-                        Delete
-                      </Button>
+                        title="Delete"
+                      />
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-          <h2 className="text-xl font-bold mt-4 mb-2">Robots</h2>
-          <div className="overflow-x-auto">
-            <table className="table-auto w-full border-collapse border border-gray-400">
-              <thead>
+        </div>
+        <div className="w-full lg:w-1/2 pr-4">
+          <h2 className="text-xl font-bold mb-2 text-gray-700">Robots</h2>
+          <div className="overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
                 <tr>
-                  <th className="px-4 py-2">Robot Name</th>
-                  <th className="px-4 py-2">Robot Status</th>
-                  <th className="px-4 py-2">Robot Charge</th>
-                  <th className="px-4 py-2">Robot Velocity</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Robot Name
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Robot Status
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Robot Charge
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Robot Velocity
+                  </th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="bg-white divide-y divide-gray-200">
                 {robots.map((robot, index) => (
                   <tr
                     key={index}
-                    className={index % 2 === 0 ? "bg-white" : "bg-gray-100"}
+                    className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
                   >
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
                       {robot.robotName}
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
                       {robot.robotStatus}
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
                       {robot.robotCharge}
                     </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {robot.robotVelocity.linearVelocity},
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
+                      {robot.robotVelocity.linearVelocity},{" "}
                       {robot.robotVelocity.angularVelocity}
                     </td>
                   </tr>
