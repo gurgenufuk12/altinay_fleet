c32eeefcd10fca1765a3ddc42b658c00b2a6ee46
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-01 14:07:37 +0300

    del: unnecessary imports

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import axios from "axios";
 import Map from "../components/Map";
-import TaskTable from "./tasks/taskTable";
 import Sidebar from "../components/SideBar";
 
 const Dashboard = () => {
