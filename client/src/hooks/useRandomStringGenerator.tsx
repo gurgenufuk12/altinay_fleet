c05b041b64091a9d5310a780827dc40bd8a75f6f
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-01 14:15:21 +0300

    feat: random string generetor for unique task and locaiton Id's

---
+++ client/src/hooks/useRandomStringGenerator.tsx
@@ -0,0 +1,18 @@
+import { useCallback } from "react";
+
+const useRandomStringGenerator = () => {
+  const generateRandomString = useCallback((jobType: string): string => {
+    const min = 1000000000000000;
+    const max = 9999999999999999;
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (jobType === "task") {
+      return "T" + randomNumber.toString();
+    }
+    return "L" + randomNumber.toString();
+  }, []);
+
+  return { generateRandomString };
+};
+
+export default useRandomStringGenerator;
