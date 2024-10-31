bed713866fea5b781c3d9848c568be06527175b2
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-31 12:13:35 +0300

    remove unsued function

--- server/controllers/robots.js
+++ server/controllers/robots.js
@@ -144,60 +144,6 @@ exports.getRobotInfo = async (req, res, next) => {
 //     });
 //   } catch (error) {}
 // };
-// exports.addTarget = async (req, res, next) => {
-//   try {
-//     const {
-//       taskName,
-//       taskCode,
-//       taskId,
-//       taskPriority,
-//       taskPercentage,
-//       robotName,
-//       linearVelocity,
-//       angularVelocity,
-//       targets,
-//       robotStatus,
-//     } = req.body;
-//     const robot = await Robot.findOne({ robotName: robotName });
-//     if (!robot) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "Robot not found",
-//       });
-//     }
-//     robot.robotStatus = robotStatus;
-//     robot.Targets = targets.map((target) => ({
-//       Position: target.targetPosition,
-//       Orientation: target.targetOrientation,
-//       targetExecuted: target.targetExecuted,
-//       locationName: target.locationName,
-//       locationDescription: target.locationDescription,
-//     }));
-//     robot.Task = {
-//       taskName: taskName,
-//       taskCode: taskCode,
-//       taskPriority: taskPriority,
-//       taskPercentage: taskPercentage,
-//       taskId: taskId,
-//     };
-//     robot.robotVelocity = {
-//       linearVelocity: linearVelocity,
-//       angularVelocity: angularVelocity,
-//     };
-
-//     await robot.save();
-//     res.status(200).json({
-//       success: true,
-//       data: robot,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(400).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// };
 // exports.updateRobotInfo = async (req, res, next) => {
 //   try {
 //     const { _id } = req.params;
