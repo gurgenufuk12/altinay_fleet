fb80cb3d26b0edc1d7748c4e3a6065c321fcdbe5
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-11-05 15:34:39 +0300

    delete unused route decleration

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,12 +4,9 @@ const router = express.Router();
 const { addTasks } = require("../controllers/tasks");
 const { deleteTask } = require("../controllers/tasks");
 const { updateSavedTask } = require("../controllers/tasks");
-// const { updateTask } = require("../controllers/tasks");
-// const { updateTaskEndTime } = require("../controllers/tasks");
+
 router.post("/addTasks", addTasks);
 router.put("/deleteTask/:taskId", deleteTask);
 router.put("/updateSavedTask/:taskId", updateSavedTask);
-// router.put("/updateTask/:taskId", updateTask);
-// router.put("/updateTaskEndTime/:taskId", updateTaskEndTime);
 
 module.exports = router;
