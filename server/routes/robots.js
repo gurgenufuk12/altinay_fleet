c7937a199505b450971f300d4b6e2b554187b9f9
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-11-05 15:55:11 +0300

    remove unused routes

--- server/routes/robots.js
+++ server/routes/robots.js
@@ -3,12 +3,8 @@ const router = express.Router();
 
 const { addTarget } = require("../controllers/robots");
 const { addRobot } = require("../controllers/robots");
-// const { getCurrentRobotVelocity } = require("../controllers/robots");
-// const { updateRobotInfo } = require("../controllers/robots");
 
 router.post("/addTarget", addTarget);
 router.post("/addRobot", addRobot);
-// router.get("/getCurrentRobotVelocity/:robotName", getCurrentRobotVelocity);
-// router.put("/updateRobotInfo/:_id", updateRobotInfo);
 
 module.exports = router;
